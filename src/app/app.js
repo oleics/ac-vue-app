793b550ae6d92285106c767e0d8cdd73ae10adea
Oliver Leics <oliver.leics@gmail.com>
2018-05-22 07:55:22 +0200

    Mount app.vue of a package as root, not some wrapper.

--- src/app/app.js
+++ src/app/app.js
@@ -3,7 +3,9 @@ var Vue = require('ac-sr')(require('vue'));
 Vue.use(require('vue-async-computed'));
 
 Vue.component('action', require('ac-sr')(require('./components/action/action.vue')));
-Vue.component('app', require('ac-sr')(require('./app-package.js')));
+
+var App = require('ac-sr')(require('./app-package.js'));
+// Vue.component('app', App);
 
 console.log(__pkg_name__, __pkg_version__);
 console.log(Object.keys(Vue.options.components));
@@ -15,28 +17,29 @@ console.log(Object.keys(Vue.options.components));
 module.exports = createApp;
 
 function createApp() {
-  return new Vue({
-    render: function render(h) {
-      return h(
-        'div',
-        {
-          attrs: {
-            id: __pkg_name__
-          }
-        },
-        [
-          h(
-            'action',
-            [
-              h(
-                'app',
-                this.$slots.default
-              )
-            ]
-          )
-        ]
-      );
-      // return h(AppWrapper, this.$slots.default);
-    }
-  });
+  return new Vue(App);
+  // return new Vue({
+  //   render: function render(h) {
+  //     return h(
+  //       'div',
+  //       {
+  //         attrs: {
+  //           id: __pkg_name__
+  //         }
+  //       },
+  //       [
+  //         h(
+  //           'action',
+  //           [
+  //             h(
+  //               'app',
+  //               this.$slots.default
+  //             )
+  //           ]
+  //         )
+  //       ]
+  //     );
+  //     // return h(AppWrapper, this.$slots.default);
+  //   }
+  // });
 }
